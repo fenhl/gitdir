e5bf512166919f062de3ce5310ebbb44d7e76b4c
Fenhl <fenhl@fenhl.net>
2016-01-11 05:18:46 +0000

    Ignore symlinks when iterating over all host dirs

--- gitdir/host/__init__.py
+++ gitdir/host/__init__.py
@@ -26,7 +26,7 @@ class Host(abc.ABC):
 
 def all():
     for host_dir in gitdir.GITDIR.iterdir():
-        if not host_dir.name.startswith('.'): # ignore dotfiles
+        if not host_dir.is_symlink() and not host_dir.name.startswith('.'): # ignore symlinks and dotfiles
             yield by_name(host_dir.name)
 
 def by_name(hostname):
