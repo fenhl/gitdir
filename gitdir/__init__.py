74feef6094d884b0116fef895885aa47233801c1
Fenhl <fenhl@fenhl.net>
2018-06-09 15:36:24 +0000

    Fix GITDIR constant to use local gitdir if global doesn't exist

--- gitdir/__init__.py
+++ gitdir/__init__.py
@@ -1,4 +1,12 @@
 import os
 import pathlib
 
-GITDIR = pathlib.Path(os.environ.get('GITDIR', '/opt/git')) #TODO check permissions
+GLOBAL_GITDIR = pathlib.Path('/opt/git')
+LOCAL_GITDIR = pathlib.Path.home() / 'git'
+
+if 'GITDIR' in is.environ:
+    GITDIR = pathlib.Path(os.environ['GITDIR'])
+elif LOCAL_GITDIR.exists() and not GLOBAL_GITDIR.exists(): #TODO check permissions
+    GITDIR = LOCAL_GITDIR
+else:
+    GITDIR = GLOBAL_GITDIR
