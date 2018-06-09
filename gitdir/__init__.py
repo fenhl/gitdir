6b4ff034a607d3e4c42760cd610a4eae84e9ab4e
Fenhl <fenhl@fenhl.net>
2018-06-09 15:41:28 +0000

    Fix typo

--- gitdir/__init__.py
+++ gitdir/__init__.py
@@ -4,7 +4,7 @@ import pathlib
 GLOBAL_GITDIR = pathlib.Path('/opt/git')
 LOCAL_GITDIR = pathlib.Path.home() / 'git'
 
-if 'GITDIR' in is.environ:
+if 'GITDIR' in os.environ:
     GITDIR = pathlib.Path(os.environ['GITDIR'])
 elif LOCAL_GITDIR.exists() and not GLOBAL_GITDIR.exists(): #TODO check permissions
     GITDIR = LOCAL_GITDIR
